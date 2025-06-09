a58c73121f98fe73c2b88602a7576571da325337
OTAVIO BARBOSA ANDRETTA <96994962+IntelAthom1Ghz@users.noreply.github.com>
2025-06-09 13:13:51 -0300

    FLASK
    starting studies about flask, creating and integrating an log-in interface with flask, html and css (no js yet)

--- estudos/__init__.py
+++ estudos/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+import os
 
-def criar_app():
+def create_app():
     app = Flask(__name__)
 
+    app.secret_key ='chave_super_secreta'
+
+    #banco de dados aqui
+    from .database import init_db
+    init_db()
+
+    #importar e registrar as rotas do app
     from .views import views
     app.register_blueprint(views)
 
--- estudos/views.py
+++ estudos/views.py
@@ -1,9 +1,58 @@
-# views.py
-from flask import Flask, render_template
+from flask import Blueprint, render_template, request, redirect,url_for, session
+import sqlite3
+import hashlib
 
-app = Flask(__name__)
+views = Blueprint('views', __name__)
 
-@app.route('/')
-def home():
-       return render_template('home.html')
-      
+#redireciona a pessoa que acesssar a raiz do site, direto pro login
+@views.route('/')
+def index():
+    return redirect(url_for('views.login')) #redireciono o usuario para o views.login
+def gerar_hash(senha):
+        return hashlib.sha256(senha.encode()).hexdigest()
+@views.route('/login', methods =['GET', 'POST'])
+def login():
+    if request.method =='POST':
+        email = request.form['email']
+        senha = request.form['senha']
+        senha_hash = gerar_hash(senha)
+        with sqlite3.connect('app/usuarios.db') as conn:
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM usuarios WHERE email = ? and SENHA = ?', (email, senha_hash))
+            usuario = cursor.fetchone()
+
+            if usuario:
+                session['usuario'] = usuario[1] #nome guardado na session
+                return redirect(url_for('views.dashboard')) #login sbem sucedido, manda pra dashboard do site
+            else:
+                return 'Login Inválido'
+    return render_template('login.html')
+
+@views.route('/cadastro', methods =['GET','POST'])
+def cadastro():
+    if request.method =='POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        senha = request.form['senha']
+        senha_hash = gerar_hash(senha)
+
+        with sqlite3.connect('app/usuarios.db') as conn:
+            cursor = conn.cursor()
+            try:
+                cursor.execute('INSERT INTO usuarios (nome, email, senha) VALUES (?,?,?)', (nome, email, senha_hash))
+                conn.commit()
+                return redirect(url_for('views.login'))
+            except sqlite3.IntegrityError:
+                return 'Email já cadastrado!'
+    return render_template('cadastro.html')
+
+@views.route('/dashboard')
+def dashboard():
+    if 'usuario' in session:
+        return render_template('dashboard.html', nome = session['usuario'])
+    return redirect(url_for('views.login'))
+@views.route('/logout')
+def logout():
+    session.pop('usuario', None)
+    return redirect(url_for('views.login'))
+    
